abc49ca2d25d4672d8d2c6e94963950b8ad8bfe3
Fabian Koller <github@kthxbye.cyou>
2024-11-20 12:03:02 +0100

    unify m1 and ergo layers

--- keyboards/monsgeek/m1/keymaps/C0nsultant/keymap.c
+++ keyboards/monsgeek/m1/keymaps/C0nsultant/keymap.c
@@ -18,24 +18,21 @@
 
 enum __layers { WIN_B, WIN_U, WIN_F, MAC_B, MAC_U, MAC_F };
 
-#define LED_LGUI 75
-#define LED_LALT 76
-
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     switch (get_highest_layer(layer_state | default_layer_state)) {
         case WIN_B:
         case WIN_U:
         case WIN_F:
-            rgb_matrix_set_color(LED_LGUI, RGB_CORAL);
+            rgb_matrix_set_color(77, RGB_BLUE);
             break;
         case MAC_B:
         case MAC_U:
         case MAC_F:
-            rgb_matrix_set_color(LED_LALT, RGB_CORAL);
+            rgb_matrix_set_color(77, RGB_GREEN);
             break;
         default:
-            rgb_matrix_set_color(LED_LGUI, RGB_RED);
-            rgb_matrix_set_color(LED_LALT, RGB_RED);
+            rgb_matrix_set_color(77, RGB_RED);
+            rgb_matrix_set_color(77, RGB_RED);
             break;
     }
     return false;
@@ -136,7 +133,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
        MO(WIN_U),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,WIN_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT,MO(WIN_F),KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT,MO(WIN_F),KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
     [WIN_U] = LAYOUT_all( /* Umlauts */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -144,15 +142,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, WIN_EUR, _______, _______, _______, WIN_UE,  _______, WIN_OE,  _______, _______, _______, _______,          _______,
         _______, WIN_AE,  WIN_SZ,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-        _______, _______, _______,                   _______,                            _______,MO(WIN_F),_______,          _______, _______, _______),
+        _______, _______, _______,                   _______,                            _______,MO(WIN_F),_______,          _______, _______, _______
+    ),
 
     [WIN_F] = LAYOUT_all( /* FN */
         QK_BOOT, KC_BRID, KC_BRIU, KC_MYCM, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,DF(MAC_B),_______, _______, _______, RGB_MOD,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RGB_MOD, RGB_HUI,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+       DF(MAC_B),_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,  RGB_VAI, _______,
-        _______, _______, _______,                   _______,                            _______, _______, _______,          RGB_SAD,  RGB_VAD, RGB_SAI),
+        _______, _______, _______,                   RGB_TOG,                            _______, _______, _______,          RGB_SAD,  RGB_VAD, RGB_SAI
+    ),
 
     [MAC_B] = LAYOUT_all( /* Base */
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MPLY,
@@ -160,7 +160,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
        MO(MAC_U),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI,MO(MAC_F),KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI,MO(MAC_F),KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
     [MAC_U] = LAYOUT_all( /* Umlauts */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -168,15 +169,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, MAC_EUR, _______, _______, _______, MAC_UE,  _______, MAC_OE,  _______, _______, _______, _______,          _______,
         _______, MAC_AE,  MAC_SZ,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-        _______, _______, _______,                   _______,                            _______,MO(MAC_F),_______,          _______, _______, _______),
+        _______, _______, _______,                   _______,                            _______,MO(MAC_F),_______,          _______, _______, _______
+    ),
 
     [MAC_F] = LAYOUT_all( /* FN */
         QK_BOOT, KC_BRID, KC_BRIU, KC_MCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,DF(WIN_B),_______, _______, _______, RGB_MOD,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RGB_MOD, RGB_HUI,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+       DF(WIN_B),_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,  RGB_VAI, _______,
-        _______, _______, _______,                   _______,                            _______, _______, _______,          RGB_SAD,  RGB_VAD, RGB_SAI)
+        _______, _______, _______,                   RGB_TOG,                            _______, _______, _______,          RGB_SAD,  RGB_VAD, RGB_SAI
+    ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
