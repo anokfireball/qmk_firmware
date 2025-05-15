502d3dcfe41b02d84ef4807174c65742198c7468
Fabian Koller <github@kthxbye.cyou>
2025-05-15 08:19:29 +0200

    voulume & media control, quick-access screen lock for NEO Ergo

--- keyboards/neo/ergo/keymaps/C0nsultant/keymap.c
+++ keyboards/neo/ergo/keymaps/C0nsultant/keymap.c
@@ -73,6 +73,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 #define WIN_UE  RALT(KC_Y)
 #define WIN_SZ  RALT(KC_S)
 #define WIN_EUR RALT(KC_5)
+#define WIN_SLP LWIN(KC_L)
 
 enum custom_keycodes {
     // avoid dead keys
@@ -151,31 +152,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true; // Process all other keycodes normally
 }
-#define MAC_SZ LALT(KC_S)
+#define MAC_SZ  LALT(KC_S)
 #define MAC_EUR LSA(KC_2)
+#define MAC_SLP LCTL(LCMD(KC_Q))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_B] = LAYOUT_hot( /* Base */
         KC_HOME, KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   WIN_6,             KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
         KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_PGDN,MO(WIN_U),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN,WIN_QUOT,           KC_ENT,
-        KC_END,  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,MO(WIN_F),
-                 KC_LCTL, KC_LGUI,          KC_LALT,          KC_SPC,                             KC_SPC,          KC_RALT,          KC_RGUI, XXXXXXX, KC_RCTL
+        KC_END,  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT,
+                 KC_LCTL, KC_LGUI,          KC_LALT,          KC_SPC,                             KC_SPC,          KC_RALT,         MO(WIN_F),KC_RGUI, KC_RCTL
     ),
 
     [WIN_U] = LAYOUT_hot( /* Umlauts */
         _______, WIN_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL,
         _______, _______, _______, _______, WIN_EUR, _______, _______,                   _______,  WIN_UE, _______,  WIN_OE, _______, _______, _______, _______,
         _______, _______, WIN_AE,  WIN_SZ,  _______, _______, _______,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,MO(WIN_F),
+        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
                  _______, _______,          _______,          _______,                            _______,          _______,          _______, _______, _______
     ),
 
     [WIN_F] = LAYOUT_hot( /* FN */
-        _______, QK_BOOT, KC_BRID, KC_BRIU, KC_MYCM, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, QK_BOOT, KC_BRID, KC_BRIU, KC_MYCM, _______, _______, _______,          _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
         _______,DF(MAC_B),_______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, WIN_SLP,
                  _______, _______,          _______,          RGB_TOG,                            _______,          _______,          _______, _______, _______
     ),
 
@@ -183,23 +185,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_HOME, KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
         KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_PGDN,MO(MAC_U),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
-        KC_END,  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,MO(MAC_F),
-                 KC_LCTL, KC_LALT,          KC_LGUI,          KC_SPC,                             KC_SPC,          KC_RGUI,          KC_RALT, _______, KC_RCTL
+        KC_END,  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT,
+                 KC_LCTL, KC_LOPT,          KC_LCMD,          KC_SPC,                             KC_SPC,          KC_RCMD,         MO(MAC_F),KC_ROPT, KC_RCTL
     ),
 
     [MAC_U] = LAYOUT_hot( /* Umlauts */
-        _______, WIN_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL,
+        _______,  KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL,
         _______, _______, _______, _______, MAC_EUR, _______, _______,                   _______, MAC_UE,  _______, MAC_OE,  _______, _______, _______, _______,
         _______, _______, MAC_AE,  MAC_SZ,  _______, _______, _______,                   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, MO(MAC_F),
+        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
                  _______, _______,          _______,          _______,                            _______,          _______,          _______, _______, _______
     ),
 
     [MAC_F] = LAYOUT_hot( /* FN */
-        _______, QK_BOOT, KC_BRID, KC_BRIU, KC_MCTL, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, QK_BOOT, KC_BRID, KC_BRIU, KC_MCTL, _______, _______, _______,          _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
         _______,DF(WIN_B),_______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, MAC_SLP,
                  _______, _______,          _______,          RGB_TOG,                            _______,          _______,          _______, _______, _______
     )
 };
